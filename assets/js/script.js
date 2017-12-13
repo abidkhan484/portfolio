966b961cfecba9a00c41f2a5eab24f1e7b5a1ea5
abidkhan <abidkhan484@gmail.com>
2017-12-13 14:08:39 +0600

    Dec 13, 2017

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+                
+                $("#about_scroll").fadeOut();   
+                $("#work_scroll").fadeOut();
+                $("#skills_scroll").fadeOut();
+
+                $("#about").click(function(){
+                    $("#index").fadeOut();
+                    $("#about_scroll").fadeIn();
+                    $('#about_left').addClass('animated slideInLeft');
+                    $('#about_right').addClass('animated slideInRight');
+                    });
+                $("#work").click(function(){
+                    $("#index").fadeOut();
+                    $("#work_scroll").fadeIn();
+                    $('#work_left').addClass('animated slideInLeft');
+                    $('#work_right').addClass('animated slideInRight');
+                    });
+                $("#skills").click(function(){
+                    $("#index").fadeOut();
+                    $("#skills_scroll").fadeIn();
+                    $('#skills_left').addClass('animated slideInLeft');
+                    $('#skills_right').addClass('animated slideInRight');
+                    });
+                
+                $(".back").click(function(){
+                    $(".pages").fadeOut();
+                    $("#index").fadeIn();
+                    $('#index_left').addClass('animated slideInLeft');
+                    $('#index_right').addClass('animated slideInRight');
+                    });
+           
+		});
